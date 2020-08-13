907d6127a18b24b4ba9abd72a4a35433dc8eae19
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-14 00:15:46 +0900

    no message

--- fc_community/fcuser/admin.py
+++ fc_community/fcuser/admin.py
@@ -5,7 +5,7 @@ from .models import Fcuser
 
 
 class FcuserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'password')
+    list_display = ('username', 'useremail',  'password')
 
 
 admin.site.register(Fcuser, FcuserAdmin)
