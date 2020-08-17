691d13422a261f6db951ab81f54eb4191e193e03
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-18 00:58:53 +0900

    no message

--- fc_community/board/urls.py
+++ fc_community/board/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('register/', views.register),
+    path('list/', views.board_lost),
 
 ]
--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def board_list(request):
+    return render(request, 'board_list.html')
