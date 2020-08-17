3dc2a0c26ed1ef245fc529a42e87f0aeb31a4615
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-18 00:57:20 +0900

    no message

--- fc_community/fc_community/urls.py
+++ fc_community/fc_community/urls.py
@@ -20,6 +20,6 @@ from fcuser.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('fcuser/', include('fcuser.urls')),
-    # path('board/', include('board.urls')),
+    path('board/', include('board.urls')),
     path('', home),
 ]
