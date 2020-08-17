6efb89c7710912cb8251b5fdbc1bdc68a8583476
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-17 22:48:03 +0900

    no message

--- fc_community/fcuser/forms.py
+++ fc_community/fcuser/forms.py
@@ -13,10 +13,10 @@ class LoginForm(forms.Form):
         error_messages={
             'required': '비밀번호를 입력해주세요.'
         },
+        
         widget=forms.PasswordInput, label="비밀번호")
 
     def clean(self):
-        
         cleaned_data = super().clean()
         username = cleaned_data.get('username')
         password = cleaned_data.get('password')
