699496cf264517d13ba50b3b2afde6fd1d6f34bd
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-20 00:46:53 +0900

    no message

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.http import Http404
 # from fcuser.models import Fcuser
+from fcuser.models import Fcuser
 # from tag.models import Tag
 from .models import Board
 from .forms import BoardForm
