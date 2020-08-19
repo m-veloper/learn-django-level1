7c5ebf66a64ad8490d8f209ac20ad44bf33b6e58
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-20 00:12:55 +0900

    no message

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -4,5 +4,5 @@ from .models import Board
 # Create your views here.
 
 def board_list(request):
-    all_boards = Board.objects.all().order_by('-id')
-    return render(request, 'board_list.html')
+    boards = Board.objects.all().order_by('-id')
+    return render(request, 'board_list.html', {'boards': boards})
