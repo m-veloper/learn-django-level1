1bce78bf72139ea4eb51f1a9f2376444dcaa0f62
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-20 00:10:06 +0900

    no message

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Board
 
 # Create your views here.
 
 def board_list(request):
+    all_boards = Board.objects.all().order_by('-id')
     return render(request, 'board_list.html')
