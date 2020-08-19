d5e6b533f0dc687c9eaeaee49fdab8fe8584a49a
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-20 00:27:18 +0900

    no message

--- fc_community/board/forms.py
+++ fc_community/board/forms.py
@@ -1,33 +1,16 @@
 from django import forms
-from .models import Fcuser
-from django.contrib.auth.hashers import check_password
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(
+class BoardForm(forms.Form):
+    title = forms.CharField(
         error_messages={
-            'required': '아이디를 입력해주세요.'
+            'required': '제목을 입력해주세요.'
         },
-        max_length=32, label="사용자 이름")
-    password = forms.CharField(
+        max_length=128, label="제목")
+    contents = forms.CharField(
         error_messages={
-            'required': '비밀번호를 입력해주세요.'
+            'required': '내용을 입력해주세요.'
         },
-        widget=forms.PasswordInput, label="비밀번호")
-
-    def clean(self):
-        cleaned_data = super().clean()
-        username = cleaned_data.get('username')
-        password = cleaned_data.get('password')
-
-        if username and password:
-            try:
-                fcuser = Fcuser.objects.get(username=username)
-            except Fcuser.DoesNotExist:
-                self.add_error('username', '아이디가 없습니다')
-                return
-
-            if not check_password(password, fcuser.password):
-                self.add_error('password', '비밀번호를 틀렸습니다')
-            else:
-                self.user_id = fcuser.id
+        widget=forms.Textarea, label="내용")
+    tags = forms.CharField(
+        required=False, label="태그")
